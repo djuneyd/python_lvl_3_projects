38d34bc0e8f7ea0285c9e83ebccf7b4cb22ade83
djuneyd <djuneyd2009@gmail.com>
2024-06-08 11:06:38 +0400

    работает

--- quiz/main.py
+++ quiz/main.py
@@ -5,21 +5,18 @@ from collections import defaultdict
 from logic import quiz_questions
 
 user_responses = {} 
+num_of_questions = {}
 # Задание 8 - создай словарь points для сохранения количества очков пользователя
 points = defaultdict(int)
 
 bot = telebot.TeleBot(token)
 
-def send_question(chat_id):
-    bot.send_message(chat_id, {quiz_questions[user_responses[chat_id]].text} , reply_markup=quiz_questions[user_responses[chat_id]].gen_markup())
-
-i=1
-us = ''
+def send_question(chat_id, usid, name):
+    bot.send_message(chat_id, {quiz_questions[user_responses[usid]].text + ' ' + '@' + str(name) + ' tg id: ' + str(usid)}, reply_markup=quiz_questions[user_responses[usid]].gen_markup())
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    global i, us
-    if call.from_user.id == us:
-        i+=1
+    if str(call.from_user.id) in call.message.text:
+        num_of_questions[call.from_user.id] += 1
         if call.data == "correct":
             bot.answer_callback_query(call.id, "Answer is correct")
             # Задание 9 - добавь очки пользователю за правильный ответ
@@ -29,32 +26,36 @@ def callback_query(call):
         bot.delete_message(call.message.chat.id, call.message.message_id-1)
         bot.delete_message(call.message.chat.id, call.message.message_id)
         # Задание 5 - реализуй счетчик вопросов
-        user_responses[call.message.chat.id]+=1
+        user_responses[call.from_user.id]+=1
         # Задание 6 - отправь пользователю сообщение с количеством его набранных очков, если он ответил на все вопросы, а иначе отправь следующий вопрос
-        if user_responses[call.message.chat.id]>=len(quiz_questions):
-            bot.send_message(call.message.chat.id, f"The end, your points: {points[call.from_user.id]}")
+        if user_responses[call.from_user.id]>=len(quiz_questions):
+            bot.send_message(call.message.chat.id, f"The end, {call.message.text.split()[-4] + ' ' + call.message.text.split()[-2] + ' ' + call.message.text.split()[-1]} points: {points[call.from_user.id]}")
         else:
-            photo = open(f'quiz\sk{i}.jpg', 'rb')
+            photo = open(f'quiz\sk{num_of_questions[call.from_user.id]}.jpg', 'rb')
             bot.send_photo(call.message.chat.id, photo)
-            send_question(call.message.chat.id)
+            send_question(call.message.chat.id, call.from_user.id, call.from_user.username)
+    else:
+        bot.send_message(call.message.chat.id, f'''ЭТОТ КВИЗ ПРИНАДЛЕЖИТ {call.message.text.split()[-4] + ' ' + call.message.text.split()[-2] + ' ' + call.message.text.split()[-1]}
+Нажмите /start чтобы начать свой квиз❗''')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    global i, us
-    if message.chat.id in user_responses.keys():
-        if i==4:
-            del user_responses[message.chat.id]
+    global i, us, usname
+    if message.from_user.id in user_responses.keys():
+        if num_of_questions[message.from_user.id]==4:
+            del user_responses[message.from_user.id]
+            del num_of_questions[message.from_user.id]
         else:
-            bot.send_message(message.chat.id, "СНАЧАЛА ЗАВЕРШИ ПРЕДЫДУЩИЙ КВИЗ!!!")
+            bot.send_message(message.chat.id, f"СНАЧАЛА ЗАВЕРШИ ПРЕДЫДУЩИЙ КВИЗ!!! @{message.from_user.username} id: {message.from_user.id}")
             bot.delete_message(message.chat.id, message.message_id)
-    if message.chat.id not in user_responses.keys():
-        user_responses[message.chat.id] = 0
+    if message.from_user.id not in user_responses.keys():
+        usname = message.from_user.username
+        user_responses[message.from_user.id] = 0
         points[message.from_user.id] = 0
-        i = 1
-        us = message.from_user.id
-        photo = open(f'quiz\sk{i}.jpg', 'rb')
+        num_of_questions[message.from_user.id] = 1
+        photo = open(f'quiz\sk{num_of_questions[message.from_user.id]}.jpg', 'rb')
         bot.send_photo(message.chat.id, photo)
-        send_question(message.chat.id)
+        send_question(message.chat.id, message.from_user.id, message.from_user.username)
 
 
 bot.infinity_polling()
