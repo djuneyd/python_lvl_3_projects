ae9802c281a43152b2bafaada4d354fa49c83203
djuneyd <djuneyd2009@gmail.com>
2024-06-08 16:04:29 +0400

    rg

--- AI_translator/bot.py
+++ AI_translator/bot.py
@@ -1,43 +1,35 @@
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 from config import token
+from logic import *
 
-# Задание №3
-from translate import translator
-import requests
-from collections import defaultdict
+bot = telebot.TeleBot(token)
 
-# Задание №5
+def gen_markup_for_text():
+        markup = InlineKeyboardMarkup()
+        markup.row_width = 1
+        markup.add(InlineKeyboardButton('Получить ответ', callback_data='text_ans'),
+                   InlineKeyboardButton('Перевести сообщение', callback_data='text_translate'))
+        
+        return markup
 
-class TextAnalysis():   
-    
-    # Задание №1
-    memory = defaultdict(list)
-    def __init__(self, text, owner):
 
-        # Задание №2
-        TextAnalysis.memory[owner].append(self)
-        self.text = text
-        self.translation = self.__translate(self.text, "ru", "en")
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if "text" in call.data:
+        obj = TextAnalysis.memory[call.from_user.username][-1]
+        if call.data == "text_ans":
+            bot.send_message(call.message.chat.id, obj.response)
+        elif call.data == "text_translate":
+            bot.send_message(call.message.chat.id,  obj.translation)
 
-        # Задание №6
-        self.response = self.get_answer()
 
-    
-    def get_answer(self):
-        res = self.__translate(self.__deep_pavlov_answer(), "en", "ru")
-        return res
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    # Дополнительное задание
+    bot.send_chat_action(message.chat.id, 'typing')
+    TextAnalysis(message.text, message.from_user.username)
+    bot.send_message(message.chat.id, "Я получил твое сообщение! Что ты хочешь с ним сделать?", reply_markup=gen_markup_for_text())
 
-    def __translate(self, text, from_lang, to_lang):
-        try:
-            # Задание №4
-        except:
-            return "Перевод не удался"
-
-    def __deep_pavlov_answer(self):
-        try:
-            API_URL = "https://7038.deeppavlov.ai/model"
-            data = {"question_raw": [ self.translation ]}
-            res = requests.post(API_URL, json=data).json()
-            res = res[0][0]
-        except:
-            res = "I don't know how to help"
-        return res
+bot.infinity_polling(none_stop=True)
