827a6c82ef36ecdb26fe6e2ef4f3239f99aa4c4a
djuneyd <djuneyd2009@gmail.com>
2024-06-08 15:49:52 +0400

    a

---
+++ AI_translator/bot.py
@@ -0,0 +1,43 @@
+from config import token
+
+# Задание №3
+from translate import translator
+import requests
+from collections import defaultdict
+
+# Задание №5
+
+class TextAnalysis():   
+    
+    # Задание №1
+    memory = defaultdict(list)
+    def __init__(self, text, owner):
+
+        # Задание №2
+        TextAnalysis.memory[owner].append(self)
+        self.text = text
+        self.translation = self.__translate(self.text, "ru", "en")
+
+        # Задание №6
+        self.response = self.get_answer()
+
+    
+    def get_answer(self):
+        res = self.__translate(self.__deep_pavlov_answer(), "en", "ru")
+        return res
+
+    def __translate(self, text, from_lang, to_lang):
+        try:
+            # Задание №4
+        except:
+            return "Перевод не удался"
+
+    def __deep_pavlov_answer(self):
+        try:
+            API_URL = "https://7038.deeppavlov.ai/model"
+            data = {"question_raw": [ self.translation ]}
+            res = requests.post(API_URL, json=data).json()
+            res = res[0][0]
+        except:
+            res = "I don't know how to help"
+        return res
---
+++ AI_translator/config.py
@@ -0,0 +1,1 @@
+token = '6863396466:AAEmmPaXeDRnpSgCk5R1k0otmBvlfPRFMt4'
