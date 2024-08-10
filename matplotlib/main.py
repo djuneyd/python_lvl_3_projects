1701bed0a6e979a68c83ade570910da30f639627
djuneyd <djuneyd2009@gmail.com>
2024-08-10 16:18:51 +0400

    biba

--- matplotlib/main.py
+++ matplotlib/main.py
@@ -1,19 +1,47 @@
+# Импортируем библиотеку matplotlib.pyplot для создания графиков
 import matplotlib.pyplot as plt
-import numpy as np
+# Импортируем библиотеку random для генерации случайных чисел
+import random
+import sqlite3
 
-b = np.matrix([[1, 2], [3, 4]])
-b_asarray = np.asarray(b)
+def get_data(start_date, end_date):
+    con = sqlite3.connect('matplotlib\data.db')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM data WHERE date BETWEEN ? AND ? ORDER BY date ", (start_date, end_date))
+    res = cur.fetchall()
+    con.close()
+    dates = [res[i][0] for i in range(len(res))]
+    prices = [res[i][1] for i in range(len(res))]
+    return dates, prices
 
-np.random.seed(19680801)  # seed the random number generator.
-data = {'a': np.arange(50),
-        'c': np.random.randint(0, 50, 50),
-        'd': np.random.randn(50)}
-data['b'] = data['a'] + 10 * np.random.randn(50)
-data['d'] = np.abs(data['d']) * 100
+dates, prices = get_data('2022-01-01', '2022-01-11')
 
-fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
-ax.scatter('a', 'b', c='c', s='d', data=data)
-ax.set_xlabel('entry a')
-ax.set_ylabel('entry b')
+# Настройка размера фигуры графика (ширина 8 дюймов, высота 6 дюймов)
+plt.figure(figsize=(8, 6))
 
-plt.show()  
+# Создание линейного графика: по оси X идут числа от 1 до 10, по оси Y - случайные числа
+# marker='o' означает, что каждое значение будет отмечено круглым маркером
+# color='r' задает красный цвет линии
+# linestyle='--' задает стиль линии (пунктирная)
+
+
+plt.plot(dates, prices, marker='o', color='g', linestyle='--')
+
+# Задаем заголовок графика
+plt.title('Курс доллара')
+
+# Задаем название оси X
+plt.xlabel('Даты')
+
+# Задаем название оси Y
+plt.ylabel('Цена')
+
+# Включаем сетку на графике для удобства восприятия данных
+plt.grid(True)
+
+# сохранение графика
+plt.savefig('matplotlib/figure.png')
+
+# Отображаем график
+plt.show()
+
