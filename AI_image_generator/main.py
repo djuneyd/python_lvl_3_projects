08cbb18c3070eaaec504b8535c8f45fb94e5edab
djuneyd <djuneyd2009@gmail.com>
2024-09-07 21:41:50 +0400

    ai generator fusion brain

---
+++ AI_image_generator/main.py
@@ -0,0 +1,64 @@
+import json
+import time
+import base64 # importing this library will allow us to decode image
+import requests
+
+# documentation --> https://fusionbrain.ai/docs/en/doc/api-dokumentaciya/
+# the programm is working with fusion brain api
+class Text2ImageAPI:
+
+    def __init__(self, url, api_key, secret_key):
+        self.URL = url
+        self.AUTH_HEADERS = {
+            'X-Key': f'Key {api_key}',
+            'X-Secret': f'Secret {secret_key}',
+        }
+
+    def get_model(self):
+        response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
+        data = response.json()
+        return data[0]['id']
+
+    def generate(self, prompt, model, images=1, width=1024, height=1024): #properties of the image
+        params = {
+            "type": "GENERATE",
+            "numImages": images,
+            "width": width,
+            "height": height,
+            "generateParams": {
+                "query": f"{prompt}"
+            }
+        }
+
+        data = {
+            'model_id': (None, model),
+            'params': (None, json.dumps(params), 'application/json')
+        }
+        response = requests.post(self.URL + 'key/api/v1/text2image/run', headers=self.AUTH_HEADERS, files=data)
+        data = response.json()
+        return data['uuid']
+
+    def check_generation(self, request_id, attempts=10, delay=10):
+        while attempts > 0:
+            response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
+            data = response.json()
+            if data['status'] == 'DONE':
+                return data['images']
+
+            attempts -= 1
+            time.sleep(delay)
+
+
+if __name__ == '__main__':
+    api = Text2ImageAPI('https://api-key.fusionbrain.ai/', '9EF57297B1D005F620CABC165CA5B122', '312091F088C1DDDCF18203B05B70FECA')
+    model_id = api.get_model()
+    uuid = api.generate("cat sitting on the window", model_id)
+    images = api.check_generation(uuid)
+    
+    with open('AI_image_generator/generated_images/sample.png', 'wb') as image:
+        image.write(base64.b64decode(images[0]))
+
+
+
+
+#Don't forget to specify exactly YOUR_KEY and YOUR_SECRET.
